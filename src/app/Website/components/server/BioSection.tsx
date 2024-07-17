0c40bb62389f5cdce2c7d641bc49a9c89be6c1fa
verdes ben <verdesb.devacc@gmail.com>
2024-07-17 11:53:43 +0200

    style inline ==> style scss bioSection done

--- src/app/Website/components/server/BioSection.tsx
+++ src/app/Website/components/server/BioSection.tsx
@@ -1,33 +1,13 @@
+import './BioSection.scss'
 const BioSection = () => {
     console.log()
     return (
-        <div
-            className="w-full overflow-hidden relative h-full rounded-md p-2  md:p-10   font-bold text-white bg-gradient-to-br  from-[#E2E1E1] to-[#C6C5C5]"
-            style={{backdropFilter: 'blur(10px)', border: '1px solid rgba(242, 242, 242, 0.7)'}}>
-            <h2 style={{
-                color: 'gray',
-                fontWeight: '600',
-                fontFamily: 'CooperHewitt, sans-serif',
-                fontSize: '2rem'
-            }}>Bio</h2>
-            <div style={{
-                height: 'calc(100% - 2rem)',
-                width: '100%',
-                display: 'flex',
-                flexDirection: 'column',
-                gap: '1rem',
-                marginTop: '1rem',
-                overflowY: 'scroll',
-                boxSizing: 'border-box',
-                paddingBottom: '2rem'
-            }} className="no-visible-scrollbar no-visible-scrollbar::-webkit-scrollbar">
-                <div style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    color: 'gray',
-                    fontWeight: '400',
-                    fontFamily: 'CooperHewitt, sans-serif'
-                }}>
+        <section
+            className="bioSection"
+           >
+            <h2 className="bioSection__title">Bio</h2>
+            <div  className="no-visible-scrollbar no-visible-scrollbar::-webkit-scrollbar bioSection__contentContainer">
+                <div className="bioSection__textContainer">
                     <p>Nathalie Afonso Dell’Omo (Afonso N.) participe utilement à une évolution de l’art, au travers de
                         ses approches, qui renouvellent et vivifient le concept de ce que l’on appelle aujourd’hui la «
                         peinture ethnique ».</p>
@@ -36,7 +16,7 @@ const BioSection = () => {
                         d’Australie, permet de témoigner du passé des peuples, montrer leur mémoire, les composantes
                         ancestrales des sociétés actuelles.</p>
                 </div>
-                <div style={{color: 'gray', fontWeight: '400', fontFamily: 'CooperHewitt, sans-serif'}}>
+                <div className="bioSection__textContainer">
                     <p>Nathalie Afonso Dell’Omo (Afonso N.) participe utilement à une évolution de l’art, au travers
                         de ses approches, qui renouvellent et vivifient le concept de ce que l’on appelle
                         aujourd’hui la « peinture ethnique ».</p>
@@ -46,7 +26,7 @@ const BioSection = () => {
                         les composantes ancestrales des sociétés actuelles.</p>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 export default BioSection
